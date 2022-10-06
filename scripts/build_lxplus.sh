c65e4614c2ede60dcbd79298ef53875ab610f306
Mohamed Elashri <muhammadelashri@gmail.com>
2022-10-06 08:54:48 +0000

    correct `build_lxplus.sh` file

--- scripts/build_lxplus.sh
+++ scripts/build_lxplus.sh
@@ -2,26 +2,12 @@
 
 # Author: Mohamed Elashri
 # Date:   2022-10-06
-# Description: This script is to build Allen on UC sleepy Machine
-# Path: build_sleepy.sh
-# Usage: chmod +x build_sleepy.sh && ./build_sleepy.sh
+# Description: This script is to build Allen on CERN lxplus
+# Path: build_lxplus.sh
 
-# First check if the OS is centos7 x64 machine only
-if [ -f /etc/os-release ]; then
-    . /etc/os-release
-    if [ "$ID" == "centos" ] && [ "$VERSION_ID" == "7" ] && [ "$ID_LIKE" == "rhel fedora" ] && [ "$(uname -m)" == "x86_64" ]; then
-        echo "OS is centos 7 x64"
-    else
-        echo "OS is not centos 7 x64"
-        exit 1
-    fi
-else
-    echo "OS is not centos 7 x64"
-    exit 1
-fi
 
 # Setup the environment
-source /cvmfs/lhcb.cern.ch/lib/var/lib/LbEnv/stable/linux-64/bin/activate
+source /cvmfs/lhcb.cern.ch/lib/LbEnv
 
 # Clone Allen from CERN gitlab
 git clone https://gitlab.cern.ch/Allen/Allen.git Allen
@@ -30,22 +16,14 @@ git clone https://gitlab.cern.ch/Allen/Allen.git Allen
 cd Allen
 mkdir build
 cd build
-
-# Choose if build is for CPU or GPU
-echo "Do you want to build Allen for CPU or GPU?"
-select yn in "CPU" "GPU"; do
-    case $yn in
-        CPU ) cmake -DSTANDALONE=ON -DCMAKE_TOOLCHAIN_FILE=/cvmfs/lhcb.cern.ch/lib/lhcb/lcg-toolchains/LCG_101/x86_64-centos7-clang12-opt.cmake ..
-; break;;
-        GPU ) cmake -DSTANDALONE=ON -DCMAKE_TOOLCHAIN_FILE=/cvmfs/lhcb.cern.ch/lib/lhcb/lcg-toolchains/LCG_101/x86_64-centos7-clang12+cuda11_4-opt.cmake ..; break;;
-    esac
-done
+cmake -DSTANDALONE=ON -DCMAKE_TOOLCHAIN_FILE=/cvmfs/lhcb.cern.ch/lib/lhcb/lcg-toolchains/LCG_101/x86_64-centos7-clang12-opt.cmake ..
 
 # Build the project
-make -j 16
+make -j 4
 
 # Run a test to see if it works 
 toolchain/wrapper ./Allen --sequence=hlt1_pp_default
 
 # If you want to run the test with a different toolchain, you can do
 #toolchain/wrapper ./Allen --sequence=hlt1_pp_default --toolchain=/cvmfs/lhcb.cern.ch/lib/lhcb/lcg-toolchains/LCG_101/x86_64-centos7-gcc10-opt.cmake
+
